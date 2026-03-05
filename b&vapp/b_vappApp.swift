deba0ed40a90b0a072645f7b3ab8be719bef4e88
Mustafa KARA <137824373+mustafakara349@users.noreply.github.com>
2026-03-05 16:09:47 +0300

    Initial Commit

---
+++ b&vapp/b_vappApp.swift
@@ -0,0 +1,17 @@
+//
+//  b_vappApp.swift
+//  b&vapp
+//
+//  Created by Mustafa KARA on 5.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct b_vappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ b&vappTests/b_vappTests.swift
@@ -0,0 +1,17 @@
+//
+//  b_vappTests.swift
+//  b&vappTests
+//
+//  Created by Mustafa KARA on 5.03.2026.
+//
+
+import Testing
+@testable import b_vapp
+
+struct b_vappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
